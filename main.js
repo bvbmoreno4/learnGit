746463e07adf4f6ad8b93b814bc0a9a4f65f93ee
odumy <odumy@softserveinc.com>
2023-05-09 17:47:15 +0300

    added console.log for 'fifth main.js'

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ console.log('main.js');
 console.log('second main.js');
 console.log('third main.js');
 console.log('fourth main.js');
+console.log('fifth main.js');
 // PCT-1000
 console.log('PCT-1000');
